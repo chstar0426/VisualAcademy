1e7eba97131bcb25abcd0ca231bed1af48157df5
chstar0426 <chstar0426@hanmail.net>
2020-02-11 23:42:10 +0900

    코드비하인드 및 OnInitialized 이벤트 처리

---
+++ VisualAcademy/Pages/Services.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages
+{
+    public partial class Services
+    {
+
+        private string Title = string.Empty;
+        protected override void OnInitialized()
+        {
+            Title = "서비스";
+        }
+    }
+}
