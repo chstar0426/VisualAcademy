08b26e2c39dcead1aed6634cefd18390250d9aef
chstar0426 <chstar0426@hanmail.net>
2020-02-13 07:44:31 +0900

    Condebehind를 이용한 클릭이벤트 구현

---
+++ VisualAcademy/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VisualAcademy.Pages.Portfolios
+{
+    public partial class PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerRef { get; set; }
+
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected void GotoDotNetKorea()
+        {
+            NavigationManagerRef.NavigateTo("/Index");
+
+        }
+
+    }
+}
